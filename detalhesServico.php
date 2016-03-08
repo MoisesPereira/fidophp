69d137ab8cc927fdfc0e557896ee394a98115f78
Moisés Pereira <moisesrmendes@gmail.com>
2016-03-08 11:46:26 -0300

    Adicionado as funcionalidades na pagina de detalhesde Serviço

---
+++ detalhesServico.php
@@ -0,0 +1,197 @@
+<?php
+require('header.php');
+require('Conexao.class.php');
+
+$id = $_GET['id'];
+
+$conn = Conexao::getInstace();
+$q = mysqli_query($conn, "select tc.nome, tc.email, tc.telefone, tc.celular, tc.celular2, tc.endereco, 
+                    tc.bairro, tc.cidade, tc.estado, tc.cep, tc.referencia,
+                    ss.funcionario, ss.valor, ss.forma_pagamento, ss.concluido, ss.dt_entrega, ss.observacao,
+                    ts.descricao desc_servico, ti.descricao desc_imagem 
+                    from tb_servico ss, tipo_servico ts, tb_cliente tc, tb_imagem ti
+                    where ss.id_servico = {$id}
+                    and ss.tipo_servico = ts.id_tipo_servico
+                    and tc.id_cliente = ss.tb_cliente_id_cliente
+                    and ss.id_servico = ti.tb_servico_id_servico;");
+$t = mysqli_fetch_assoc($q);
+
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post" action="./alterarServico.php">
+                    <fieldset>
+                        <legend class="text-center header">Detalhes do Serviço</legend>
+
+                        <p class="text-center header">Cliente</p>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="fname" name="fname" type="text" value="<?php echo $t['nome']; ?>" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" value="<?php echo $t['email']; ?>" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="phone" name="phone" type="text" value="<?php echo $t['telefone']; ?>" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="celular" name="celular" type="text" value="<?php echo $t['celular']; ?>" placeholder="Celular" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="celular2" name="celular2" type="text" value="<?php echo $t['celular2']; ?>" placeholder="Celular2" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="endereco" name="endereco" type="text" value="<?php echo $t['endereco']; ?>" placeholder="Endereço" class="form-control">
+                            </div>
+                        </div>
+
+                         <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="bairro" name="bairro" type="text" value="<?php echo $t['bairro']; ?>" placeholder="Bairro" class="form-control">
+                            </div>
+                        </div> 
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="cidade" name="cidade" type="text" value="<?php echo $t['cidade']; ?>" placeholder="Cidade" class="form-control">
+                            </div>
+                        </div>   
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="estado" name="estado" type="text" value="<?php echo $t['estado']; ?>" placeholder="Cidade" class="form-control">
+                            </div>
+                        </div>        
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="cep" name="cep" type="text" value="<?php echo $t['cep']; ?>" placeholder="CEP" class="form-control">
+                            </div>
+                        </div> 
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="referencia" name="referencia" type="text" value="<?php echo $t['referencia']; ?>" placeholder="Ponto de Referencia" class="form-control">
+                            </div>
+                        </div>   
+
+
+                        <p class="text-center header">Serviço</p>
+
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                            <label>Funcionario:</label>
+                                <input id="funcionario" name="funcionario" type="text" value="<?php echo $t['funcionario']; ?>" class="form-control">
+                            </div>
+                        </div> 
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                            <label>Valor:</label>
+                                <input id="valor" name="valor" type="text" value="<?php echo $t['valor']; ?>" class="form-control">
+                            </div>
+                        </div>       
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                            <label>Forma de Pagamento:</label>
+                                <input id="forma_pagamento" name="forma_pagamento" type="text" value="<?php echo $t['forma_pagamento']; ?>" class="form-control">
+                            </div>
+                        </div> 
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                            <label>Concluido:</label>
+                                <input id="concluido" name="concluido" type="text" value="<?php echo $t['concluido']; ?>" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                            <label>Data de Entrega:</label>
+                                <input id="dt_entrega" name="dt_entrega" type="text" value="<?php echo $t['dt_entrega']; ?>" class="form-control">
+                            </div>
+                        </div> 
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                            <label>Observação:</label>
+                                <input id="observacao" name="observacao" type="text" value="<?php echo $t['observacao']; ?>"  class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                            <label>Tipo:</label>
+                                <input id="desc_servico" name="desc_servico" type="text" value="<?php echo $t['desc_servico']; ?>" class="form-control">
+                            </div>
+                        </div> 
+
+                    
+
+
+Colocar umas divs para receber as imagens (Criar um for de 5 ou 10 para carregar as imagens)
+                        <div id="img">
+                            <img src="./uploads/<?=$t['desc_imagem']?>" height="100" width="100"></img>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-lg">Alterar</button>
+                                <button type="submit" class="btn btn-primary btn-lg">Deletar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+</body>
+
+</html>
--- listagemServicos.php
+++ listagemServicos.php
@@ -227,32 +227,42 @@ $(document).ready(function(){
 });
 </script>
 
+</div>
+
 <?php
 
-$id_cliente = isset($_POST['id_cliente']) ? $_POST['id_cliente'] : '';
+$id = isset($_POST['id_cliente']) ? $_POST['id_cliente'] : '';
 $funcionario = isset($_POST['funcionario']) ? $_POST['funcionario'] : '';
-$dt_entrega = isset($_POST['dt_entrega']) ? $_POST['dt_entrega'] : '';
+/*$dt_entrega = isset($_POST['dt_entrega']) ? $_POST['dt_entrega'] : '';
 $concluido = isset($_POST['concluido']) ? $_POST['concluido'] : '';
 $tp_servico = isset($_POST['tp_servico']) ? $_POST['tp_servico'] : '';
+*/
 
+//if( $id != '' || $funcionario != '' || $dt_entrega != '' || $concluido != '' || $tp_servico != '' ){
+if( $id != '' || $funcionario != '' ){
 
-if( $id_cliente != '' || $funcionario != '' || $dt_entrega != '' || $concluido != '' || $tp_servico != '' ){
-
-$qnome = ($nome != '')  ? "and nome like '%{$nome}%' " : '';
-$qemail = ($email != '') ? "and email like '%{$email}%' " : '';
-$qtelefone = ($telefone != '') ? "and telefone like '%{$telefone}%' " : '';
+$qid = ($id != '')  ? "and tb_cliente_id_cliente = {$id} " : '';
+$qfuncionario = ($funcionario != '') ? "and funcionario = '%{$funcionario}%' " : '';
+/*$qtelefone = ($telefone != '') ? "and telefone like '%{$telefone}%' " : '';
 $qData = ( empty($dtIni) && empty($dtFim) ) ? "and STR_TO_DATE('dt_cadastro','%Y-%m-%d') BETWEEN '{$dtIni[2]}-{$dtIni[1]}-{$dtIni[0]}' AND '{$dtFim[2]}-{$dtFim[1]}-{$dtFim[0]}'" : '';
+*/
 
-$queryN = "select * from tb_servico where 1 = 1 {$qnome} {$qemail} {$qtelefone} {$qData} ";
+$queryN = "select * from tb_servico ss inner join tipo_servico ts 
+    where 1 = 1 
+    {$qid} {$qfuncionario}
+    and ss.tipo_servico = ts.id_tipo_servico";
 
     ?>
     <table border="1" class="table table-hover">
         <thead>
         <tr>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Telefone</th>
-            <th>Escolha o Cliente</th>
+            <th>Funcionário</th>
+            <th>Valor</th>
+            <th>Tipo Serviço</th>
+            <th>Data Entrega</th>
+            <th>Concluido</th>
+            <th>Data Entrega</th>
+            <th>Selecione</th>
         </tr>
         </thead>
         <tbody>
@@ -262,12 +272,17 @@ $queryN = "select * from tb_servico where 1 = 1 {$qnome} {$qemail} {$qtelefone} 
     $conn = Conexao::getInstace();
     $q = mysqli_query($conn, $queryN);
 
-    while($t = mysqli_fetch_array($q)){
+    while($t = mysqli_fetch_assoc($q)){
+        //var_dump($t);
+        
         echo "<tr>";
-        echo "<td>$t[1]</td>";
-        echo "<td>$t[2]</td>";
-        echo "<td>$t[3]</td>";
-        echo "<td><a href='/detalhes.php/?id={$t[0]}'>Selecionar</a></td>";
+        echo "<td>{$t['funcionario']}</td>";
+        echo "<td>{$t['valor']}</td>";
+        echo "<td>{$t['descricao']}</td>";
+        echo "<td>{$t['dt_entrega']}</td>";
+        echo "<td>{$t['concluido']}</td>";
+        echo "<td>{$t['dt_entrega']}</td>";
+        echo "<td><a href='/detalhesServico.php?id={$t['id_servico']}'>Selecionar</a></td>";
         echo "</tr>";
     }
 
@@ -278,7 +293,5 @@ $queryN = "select * from tb_servico where 1 = 1 {$qnome} {$qemail} {$qtelefone} 
 <?php    
 }
 ?>
-          
-</div>
 </body>
 </html>
